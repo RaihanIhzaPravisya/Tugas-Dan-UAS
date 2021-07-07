67eb826370bd1ab58cd39cd655daf09406452234
Raihan Ihza Pravisya <85867165+RaihanIhzaPravisya@users.noreply.github.com>
2021-07-07 21:37:30 +0700

    Add files via upload

---
+++ Transaksi Printer _ 20083000149.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  11 10:33:40 2021
+
+@author: Raihan Ihza Pravisya
+"""
+
+#Transaksi printer
+ulang="y"
+while ulang=="y" or ulang=="Y":
+    print ("===========================")
+    print (" Program ini dibuat oleh ")
+    print (" Raihan Ihza Pravisya ")
+    print (" Menghitung Transaksi Printer ")
+    print ("===========================")
+    
+    harga=660000
+    jml=0
+    while jml>=0:
+        jml=int(input ("--> Jumlah printer yang dibeli:"))
+        if jml>=0:
+            total = harga*jml
+            print("--> Total pembayaran: Rp.", total)
+    
+            ulang = input("--> Ingin Mengulang program ini? Iya/Tidak:")
+            if ulang=="t" or ulang=="T":
+                break
+        else:
+            pesan="(Nilai inputan harus lebih dari 0)"
+            print(pesan)
+            break
