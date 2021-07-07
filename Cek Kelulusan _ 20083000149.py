8b9fedd5ffec1998283ce4d89e9315a900816f19
Raihan Ihza Pravisya <85867165+RaihanIhzaPravisya@users.noreply.github.com>
2021-07-07 21:27:10 +0700

    Add files via upload

---
+++ Cek Kelulusan _ 20083000149.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 21:24:00 2021
+
+@author: Raihan Ihza Pravisya
+"""
+
+#Cek kelulusan, jika nilai > 80 maka lulus
+ulang = "y"
+while ulang=="y" or ulang=="Y":
+    
+    print("=============================")
+    print  (" Program Ini dibuat Oleh ")
+    print   (" Raihan Ihza Pravisya ")
+    print("        Cek Kelulusan        ")
+    print("=============================")
+    
+    n=0
+    while n<=100:
+        n = int(input("--> Masukkan nilai :"))
+        if int(n<=100):
+            if int(n)>60:
+                sts = "Lulus"
+            else:
+                sts = "Tidak Lulus"
+            print ("--> Status anda:",sts)
+    
+            ulang = input("Ingin Mengulangi program? Iya/Tidak:")
+            if ulang=="t" or ulang=="T":
+                break
+        else:
+            pesan="(Nilai inputan harus kurang dari 100)"
+            print(pesan)
+            break
